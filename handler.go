7cb31af974b2cabd7dd681b6fb96cd3cf8e9b1f3
Jack Hayhurst <jakdept@gmail.com>
2017-06-03 02:39:12 -0400

    moved internal handler out to it's own files
    Signed-off-by: Jack Hayhurst <jakdept@gmail.com>

--- handler.go
+++ handler.go
@@ -4,29 +4,19 @@ package handler
 
 import (
 	"fmt"
-	"html/template"
-	"image"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
-	"time"
 
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 
-	"image/jpeg"
-	"image/png"
-
 	"github.com/jakdept/dir"
 	_ "github.com/jakdept/sp9k1/statik"
-	"github.com/nfnt/resize"
-	"github.com/oliamb/cutter"
-	"github.com/traherom/memstream"
 )
 
 // SplitHandler allows the routing of one handler at /, and another at all locations below /.
@@ -74,28 +64,6 @@ func (h dirSplitHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// InternalHandler serves a static, in memory filesystem..
-func InternalHandler(logger *log.Logger, fs http.FileSystem) http.Handler {
-	return internalHandler{handler: http.FileServer(fs), l: logger}
-}
-
-type internalHandler struct {
-	handler http.Handler
-	l       *log.Logger
-}
-
-func (c internalHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// block access to files ending in .template
-	if path.Ext(r.URL.Path) == ".template" {
-		http.Error(w, fmt.Sprintf("template requested, blocked: %s", r.URL.Path), http.StatusForbidden)
-		c.l.Printf("403 - error responding: %s", r.URL.Path)
-		return
-	}
-
-	c.handler.ServeHTTP(w, r)
-	return
-}
-
 // ContentTypeHandler serves a given file back to the requester, and determines content type by algorithm only.
 // It does not use the file's extension to determine the content type.
 func ContentTypeHandler(logger *log.Logger, basePath string) http.Handler {
@@ -107,15 +75,54 @@ type contentTypeHandler struct {
 	l        *log.Logger
 }
 
-type errorHandler struct {
+// contentTypeHandler.ServeHTTP satasifies the Handler interface.
+func (c contentTypeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open(filepath.Join(c.basePath, r.URL.Path))
+	if err != nil {
+		http.Error(w, fmt.Sprintf("not found: %s", r.URL.Path), http.StatusNotFound)
+		c.l.Printf("404 - could not open file: %s - %s", filepath.Join(c.basePath, r.URL.Path), err)
+		return
+	}
+	defer f.Close()
+
+	stat, err := f.Stat()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("cannot read file: %s", r.URL.Path), http.StatusInternalServerError)
+		c.l.Printf("500 - could not stat file: %s - %s", filepath.Join(c.basePath, r.URL.Path), err)
+		return
+	}
+
+	chunk := make([]byte, 512)
+
+	_, err = f.Read(chunk)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("cannot read file: %s", r.URL.Path), http.StatusInternalServerError)
+		c.l.Printf("500 - could not read from file: %s - %s", filepath.Join(c.basePath, r.URL.Path), err)
+		return
+	}
+
+	_, err = f.Seek(0, io.SeekStart)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("cannot read file: %s", r.URL.Path), http.StatusInternalServerError)
+		c.l.Printf("500 - could not seek within file: %s - %s", filepath.Join(c.basePath, r.URL.Path), err)
+		return
+	}
+
+	w.Header().Set("Content-Type", http.DetectContentType(chunk))
+	http.ServeContent(w, r, r.URL.Path, stat.ModTime(), f)
+
+	return
+}
+
+type responseCodeHandler struct {
 	code int
 	msg  string
 }
 
-func (h errorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h responseCodeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, h.msg, h.code)
 }
 
-func ErrorHandler(code int, msg string, args ...interface{}) http.Handler {
-	return errorHandler{code: code, msg: fmt.Sprintf(msg, args...)}
+func ResponseCodeHandler(code int, msg string, args ...interface{}) http.Handler {
+	return responseCodeHandler{code: code, msg: fmt.Sprintf(msg, args...)}
 }
