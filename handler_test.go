6898f3184c6549cf01cbe31891afb1ada26ff5ad
Jack Hayhurst <jhayhurst@liquidweb.com>
2017-06-08 17:21:39 -0400

    moved the Header tests to header_test.go
    Signed-off-by: Jack Hayhurst <jhayhurst@liquidweb.com>

--- handler_test.go
+++ handler_test.go
@@ -240,11 +240,3 @@ func TestDirSplit(t *testing.T) {
 		})
 	}
 }
-
-func TestHeader(t *testing.T) {
-	ts := httptest.NewServer(Header("superheader", "secret value", Success("yay")))
-
-	resp, err := http.Get(ts.URL)
-	assert.Nil(t, err)
-	assert.Equal(t, "secret value", resp.Header.Get("superheader"))
-}
--- header_test.go
+++ header_test.go
@@ -1,2 +1,17 @@
 package dandler
 
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHeader(t *testing.T) {
+	ts := httptest.NewServer(Header("superheader", "secret value", Success("yay")))
+
+	resp, err := http.Get(ts.URL)
+	assert.Nil(t, err)
+	assert.Equal(t, "secret value", resp.Header.Get("superheader"))
+}
